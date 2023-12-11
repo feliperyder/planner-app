1ac99f983fac86ee0d450b38020f8d0817539833
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-11 23:19:22 +0000

    Js code development

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
 
     // Event Listeners
-
+    $(".saveBtn").on("click", function () {
+    }
     // Display current day at the top of the calendar
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm A "));
 
-    // Time block for 9am-5pm
-
+    // 1 hour time blocks between 9am-5pm
+    function TimeBlocks() {
+      var container = $(".container");
+  
     // Loop thorugh hours
+    for (var hour = 9; hour <= 17; hour++)
         // create new time block element
+      var timeBlock = 
         //create hour element
-
+      var hourElement = 
         //Create text area for event details text to be inputted
-    
+      var eventDetails = 
+      
     // Set a class for Color coded time block depending on; past, present, or future
 
     // Create Save button
@@ -22,9 +28,12 @@ $(document).ready(function () {
     // Append the time block to the container with class 'container'
 
     // Save event to local storage
+    localStorage.setItem(eventHour, eventText);
 
     // Load events from local storage
-    
+      function loadEvents() {
+
+      }
       // Call the function to load events
       loadEvents();
     });
