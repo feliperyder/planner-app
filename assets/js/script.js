cdad9e0f677769023f6b53ee20ca2760a6d94e3b
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-12 18:26:39 +0000

    Js code completed, save button formatting modified & 'Clear all event details' button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,85 @@
 $(document).ready(function () {
-
-    // Event Listeners
+    // Event Listener for save button
     $(".container").on("click", ".saveBtn", function () {
       // Get the text input and the corresponding hour
-      var eventText = $(this).siblings(".description").val();
-      var eventHour = $(this).siblings(".hour").text();
-      // Save event to local storage
-      localStorage.setItem(eventHour, eventText);
+      var evtText = $(this).siblings(".description").val();
+      var evtHour = $(this).siblings(".hour").text();
+  
+      // Save the event to local storage
+      localStorage.setItem(evtHour, evtText);
     });
   
-    // Display current day at the top of the calendar
-    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm A "));
+    // Event Listener for clear button
+    var clearBtn = $("<button>")
+      .addClass("btn btn-danger mt-3")
+      .text("Fully clear Schedule")
+      .on("click", function () {
+        // Clear the local storage
+        localStorage.clear();
+        // Reload the page to reset the schedule
+        location.reload();
+      });
   
-    // 1 hour time blocks between 9am-5pm
+    // Display current day at the top of the calendar
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm A"));
+  
+    // Function to create time blocks for 9am-5pm
     function createTimeBlock() {
       var container = $(".container");
   
-      // Loop through hours
-      for (var hour = 9; hour <= 17; hour++) {
-        // create new time block element
-        var timeBlock = $("<div>")
-          .addClass("row time-block");
-        // create hour element
+      // Loop through hours from 9am to 5pm
+      for (var hr = 9; hr <= 17; hr++) {
+        // Create a new time block element
+        var timeBlock = $("<div>").addClass("row time-block");
+  
+        // Create hour element
         var hourElement = $("<div>")
           .addClass("col-md-1 hour")
-          .text(dayjs().hour(hour).format("hA"));
-        // Create text area for event details text to be inputted
-        var eventDetails = $("<textarea>")
-          .addClass("col-md-15 description");
+          .text(dayjs().hour(hr).format("hA"));
   
-        // Set a class for Color coded time block depending on; past, present, or future
-        eventDetails.addClass(
-          hour < dayjs().hour() ? "past" : hour === dayjs().hour() ? "present" : "future"
-        );
+        // Create textarea for event details
+        var eventDetails = $("<textarea>").addClass("col-md-10 description");
   
-        // Create Save button
+        // Set the class for past, present, or future based on the current time
+        if (hr < dayjs().hour()) {
+          eventDetails.addClass("past");
+        } else if (hr === dayjs().hour()) {
+          eventDetails.addClass("present");
+        } else {
+          eventDetails.addClass("future");
+        }
+  
+        // Create save button
         var saveBtn = $("<button>")
           .addClass("col-md-1 saveBtn")
           .append($("<i>").addClass("fas fa-save"));
   
         // Append elements to the time block
         timeBlock.append(hourElement, eventDetails, saveBtn);
-        // Append the time block to the container with class 'container'
+  
+        // Append the time block to the container
         container.append(timeBlock);
       }
     }
   
-    // Call function
+    // Call the function to create time blocks
     createTimeBlock();
   
-    // Load events from local storage
+    // Append clear button to the container
+    $(".container").append(clearBtn);
+  
+    // Function to load events from local storage
     function loadEvents() {
       $(".time-block").each(function () {
-        var hour = $(this).find(".hour").text();
-        var savedEvent = localStorage.getItem(hour);
+        var hr = $(this).find(".hour").text();
+        var savedEvt = localStorage.getItem(hr);
   
         // Set the saved event in the corresponding textarea
-        $(this).find(".description").val(savedEvent);
+        $(this).find(".description").val(savedEvt);
       });
     }
   
     // Call the function to load events
     loadEvents();
-  });  
+  });
+  
